c330affed10675d193fc9c1b1a36702a19bd2a02
Gabriel Torres <torresg.dev@gmail.com>
2025-06-12 08:55:58 -0300

    Update shoppingList Module, Created shoppingList service

--- backend/src/shopping-list/shopping-list.module.ts
+++ backend/src/shopping-list/shopping-list.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ShoppingListService } from './shopping-list.service';
 import { ShoppingListController } from './shopping-list.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ShoppingListItem } from './shopping-list-item.entity';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([ShoppingListItem]),
+    AuthModule,
+  ],
   providers: [ShoppingListService],
   controllers: [ShoppingListController]
 })
--- backend/src/shopping-list/shopping-list.service.ts
+++ backend/src/shopping-list/shopping-list.service.ts
@@ -1,4 +1,45 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ShoppingListItem } from './shopping-list-item.entity';
+import { CreateShoppinListItemDto } from './dto/create-shopping-list-item.dto';
+import { UpdateShoppingListItemDto } from './dto/update-shopping-list-item.dto';
 
 @Injectable()
-export class ShoppingListService {}
+export class ShoppingListService {
+    constructor(
+        @InjectRepository(ShoppingListItem)
+        private shoppingListItemRepository: Repository<ShoppingListItem>
+    ) {}
+
+    async create(createDto: CreateShoppinListItemDto, userId:string): Promise<ShoppingListItem> {
+        const item = this.shoppingListItemRepository.create({ ...createDto, userId})
+        return this.shoppingListItemRepository.save(item)
+    }
+
+    async findAll(userId: string): Promise<ShoppingListItem[]> {
+        return this.shoppingListItemRepository.find({ where: {userId}})
+    }
+
+    async findOne(id: string, userId: string): Promise<ShoppingListItem> {
+        const item = await this.shoppingListItemRepository.findOne({where: {id}})
+        if(!item) {
+            throw new NotFoundException('Item da lista de compras não encontrado.');
+        }
+        if (item.userId !== userId) {
+            throw new UnauthorizedException('Voce não tem permissao para acessar este item')
+        }
+        return item;
+    }
+
+    async update(id: string, updateDto:UpdateShoppingListItemDto, userId: string): Promise <ShoppingListItem> {
+        const item = await this.findOne(id, userId);
+        Object.assign(item, updateDto)
+        return this.shoppingListItemRepository.save(item)
+    }
+
+    async remove(id: string, userId: string): Promise<void> {
+        const item = await this.findOne(id, userId);
+        await this.shoppingListItemRepository.remove(item)
+    }
+}
